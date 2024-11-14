8627209dcfc17892d2b993ea84f8d348d8396a52
plotofdirt <53881725+plotofdirt@users.noreply.github.com>
2024-11-13 18:13:36 -0600

    Fix 1 of 2 for smoketest

--- tests/workshop/smoketest.spec.ts
+++ tests/workshop/smoketest.spec.ts
@@ -13,17 +13,23 @@ test.describe('Header Menu Visual Regression', () => {
 
     test('Visual Regression for Auctions Menu', { tag: '@smoke' }, async ({ page }) => {
         await headerPage.clickAuctions(); // Click Auctions link
-        await page.waitForTimeout(1000); // Optional: wait for content to load
+        await page.waitForLoadState() // the promise resolves after "load" event.
         await expect(page)
             .toHaveScreenshot('auctions.png', {
-            maxDiffPixels: pixelDiff, // Allow small differences
+                mask: [
+                    page.locator('#calendar-banner'),
+                    page.locator("//*[contains (@class, 'caroufredsel_wrapper')]"),     // TODO: GROSS OLD HABITS
+                    page.locator('#hpa')
+                ],
+                fullPage: true,
+                maxDiffPixels: pixelDiff, // Allow small differences
         });
     });
 
     test('Visual Regression for Buy Menu', {
         tag: '@smoke' }, async ({ page }) => {
         await headerPage.clickBuy(); // Click Buy link
-        await page.waitForTimeout(1000); // Optional: wait for content to load
+        await page.waitForLoadState() // the promise resolves after "load" event.
         await expect(page)
             .toHaveScreenshot('buy.png', {
             maxDiffPixels: pixelDiff, // Allow small differences
@@ -33,7 +39,7 @@ test.describe('Header Menu Visual Regression', () => {
     test('Visual Regression for Sell Menu', {
         tag: '@smoke' }, async ({ page }) => {
         await headerPage.clickSell(); // Click Sell link
-        await page.waitForTimeout(1000); // Optional: wait for content to load
+        await page.waitForLoadState() // the promise resolves after "load" event.
         await expect(page)
             .toHaveScreenshot('sell.png', {
             maxDiffPixels: pixelDiff, // Allow small differences
@@ -43,7 +49,7 @@ test.describe('Header Menu Visual Regression', () => {
     test('Visual Regression for Results Menu', {
         tag: '@smoke' }, async ({ page }) => {
         await headerPage.clickResults(); // Click Results link
-        await page.waitForTimeout(1000); // Optional: wait for content to load
+        await page.waitForLoadState() // the promise resolves after "load" event.
         await expect(page)
             .toHaveScreenshot('results.png', {
             maxDiffPixels: pixelDiff, // Allow small differences
@@ -53,7 +59,7 @@ test.describe('Header Menu Visual Regression', () => {
     test('Visual Regression for Equipment Menu', {
         tag: '@smoke' }, async ({ page }) => {
         await headerPage.clickEquipment(); // Click Equipment text
-        await page.waitForTimeout(1000); // Optional: wait for content to load
+        await page.waitForLoadState() // the promise resolves after "load" event.
         await expect(page)
             .toHaveScreenshot('equipment.png', {
             maxDiffPixels: pixelDiff, // Allow small differences
@@ -63,27 +69,35 @@ test.describe('Header Menu Visual Regression', () => {
     test('Visual Regression for Appraisals Menu', {
         tag: '@smoke' }, async ({ page }) => {
         await headerPage.clickAppraisals(); // Click Appraisals link
-        await page.waitForTimeout(1000); // Optional: wait for content to load
+        await page.waitForLoadState() // the promise resolves after "load" event.
         await expect(page)
             .toHaveScreenshot('appraisals.png', {
-            maxDiffPixels: pixelDiff, // Allow small differences
+                mask:[
+                    page.locator('#root > div > div:nth-child(1)')
+                ],
+                fullPage: true,
+                maxDiffPixels: pixelDiff, // Allow small differences
         });
     });
 
     test('Visual Regression for Shipping Menu', {
     tag: '@smoke' }, async ({ page }) => {
         await headerPage.clickShipping(); // Click Shipping link
-        await page.waitForTimeout(1000); // Optional: wait for content to load
+        await page.waitForLoadState() // the promise resolves after "load" event.
         await expect(page)
             .toHaveScreenshot('shipping.png', {
-            maxDiffPixels: pixelDiff, // Allow small differences
+                mask: [
+                    page.locator('#html5_video_bfprqvef1t')
+                ],
+                fullPage: true,
+                maxDiffPixels: pixelDiff, // Allow small differences
         });
     });
 
     test('Visual Regression for About Menu', {
         tag: '@smoke' }, async ({ page }) => {
         await headerPage.clickAbout(); // Click About link
-        await page.waitForTimeout(1000); // Optional: wait for content to load
+        await page.waitForLoadState() // the promise resolves after "load" event.
         await expect(page)
             .toHaveScreenshot('about.png', {
             maxDiffPixels: pixelDiff, // Allow small differences
