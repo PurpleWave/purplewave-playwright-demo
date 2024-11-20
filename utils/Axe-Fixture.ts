da07a16afe466722ec4beb48058fe789bf272af5
plotofdirt <53881725+plotofdirt@users.noreply.github.com>
2024-11-20 11:43:47 -0600

    commenting AXE implementation - NW

--- utils/Axe-Fixture.ts
+++ utils/Axe-Fixture.ts
@@ -1,20 +1,27 @@
-// axe-fixture.ts
 import { test as base, expect } from '@playwright/test';
 import AxeBuilder from '@axe-core/playwright';
 
 type AxeFixture = {
-  makeAxeBuilder: () => AxeBuilder;
+    /**
+     * Creates and configures a new AxeBuilder instance for accessibility testing.
+     * 
+     * @returns {AxeBuilder} A configured AxeBuilder instance.
+     */
+    makeAxeBuilder: () => AxeBuilder;
 };
 
-// Extend base test to include a `makeAxeBuilder` fixture
+/**
+ * Extends the Playwright base test with an `makeAxeBuilder` fixture.
+ * This fixture initializes AxeBuilder with predefined accessibility tags.
+ */
 export const test = base.extend<AxeFixture>({
-  makeAxeBuilder: async ({ page }, use) => {
-    const makeAxeBuilder = () =>
-      new AxeBuilder({ page })
-        .withTags(['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa']); // Customize tags if needed
+    makeAxeBuilder: async ({ page }, use) => {
+        const makeAxeBuilder = () =>
+            new AxeBuilder({ page })
+            .withTags(['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa']); // Customize tags if needed
 
-    await use(makeAxeBuilder);
-  },
+        await use(makeAxeBuilder);
+    },
 });
 
 export { expect };
