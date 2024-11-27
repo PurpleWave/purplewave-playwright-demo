ea4716e54a2b644feed12271b53d3022a3ce12a6
Nicholas David Wilcox <nicholas.wilcox@gmail.com>
2024-11-27 15:42:10 -0600

    saving

--- tests/ims/login.spec.ts
+++ tests/ims/login.spec.ts
@@ -1,13 +1,11 @@
 import { test, expect } from '@playwright/test';
 
 test.only('test', async ({ page }) => {
-  await page.goto('https://qa.cliquidator.info/c/');
-  await page.locator('input[name="username"]').click();
-  await page.locator('input[name="username"]').fill('nick.wilcox');
-  await page.locator('input[name="username"]').press('Tab');
-  await page.locator('input[name="password"]').fill('x.x.(Kickx.x.ball)!1');
-  await page.getByRole('button', { name: 'Sign in' }).click();
+    await page.goto('https://qa.cliquidator.info/c/');
+    await page.locator('input[name="username"]').fill('');
+    await page.locator('input[name="password"]').fill('');
+    await page.getByRole('button', { name: 'Sign in' }).click();
     await page.waitForTimeout(10000);
-  await page.goto('https://ims-qa.cliquidator.info/home');
-  await page.waitForTimeout(10000);
+    await page.goto('https://ims-qa.cliquidator.info/home');
+    await page.waitForTimeout(10000);
 });
