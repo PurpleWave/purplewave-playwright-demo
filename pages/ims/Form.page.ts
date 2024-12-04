2ba2369f2dd03ad88264c8e03d09dab421129b03
Nicholas David Wilcox <nicholas.wilcox@gmail.com>
2024-12-04 15:41:33 -0600

    initial pages

---
+++ pages/ims/Form.page.ts
@@ -0,0 +1,8 @@
+import { Page, Locator } from 'playwright';
+
+/**
+ * Form class for handling various tabs, buttons, and interactions for the form component
+ */
+export class FormPage {
+    private page: Page;
+}
---
+++ pages/ims/SideDrawer.page.ts
@@ -0,0 +1,8 @@
+import { Page, Locator } from 'playwright';
+
+/**
+ * Sidedrawer class for handling various tabs, buttons, and interactions for the side drawer component
+ */
+export class SideDrawerPage {
+    private page: Page;
+}
