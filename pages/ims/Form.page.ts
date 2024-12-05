4c7fc39fd85785cf4b3eb7eb3e75b4b78617c25f
i-kuts <irina.kuts@purplewave.com>
2024-12-05 15:39:31 -0600

    added locators for DataCapture, Form, and SideDrawer pages

--- pages/ims/Form.page.ts
+++ pages/ims/Form.page.ts
@@ -6,11 +6,34 @@ import { DataCapturePage } from './DataCapture.page';
  */
 export class FormPage extends DataCapturePage{
 
-    public example: Locator;
+    public hideFormButton: Locator;
+    public viewHistoryButton: Locator;
+    public submittedByFullText: Locator;
+    public submittedByLabel: Locator;
+    public lastUpdatedByLabel: Locator;
+    public cancelButton: Locator;
+    public saveButton: Locator;
+    //
+    public addItemToLotButton: Locator;
+    public cancelBottomButton: Locator;
+    public statusBottomDropdown: Locator;
+    public saveBottomDropdown: Locator;
 
     constructor(page: Page) {
-        super(page)
-        this.example = this.page.locator('#');
+        super(page);
+        this.hideFormButton = this.page.getByRole('button', { name: 'Hide Form' });
+        this.viewHistoryButton = this.page.getByRole('button', { name: 'View History' });
+        this.submittedByLabel = this.page.getByText('Submitted By:');
+        this.lastUpdatedByLabel = this.page.getByText('Last Updated By:');
+        this.cancelButton = this.page.locator('div').filter({ hasText: /^Cancel$/ });
+        this.saveButton = this.page.locator('div').filter({ hasText: /^Save$/ }).nth(1);
+        //
+        this.addItemToLotButton = this.page.getByRole('button', { name: 'Add Item To Lot' });
+        this.cancelBottomButton = this.page.locator('div').filter({ hasText: /^Cancel$/ }).nth(1);
+        this.statusBottomDropdown = this.page.locator('select[name="status"]').nth(1);
+        this.saveBottomDropdown = this.page.locator('div').filter({ hasText: /^Save$/ }).nth(3);
+
+
     }
 
 
